bfc67077129a698c3a3ac9e0ee9110790d073e17
thabezerra <thainasouza.sud@gmail.com>
2024-08-13 02:44:16 -0300

    Initial commit

---
+++ src/test/java/me/dio/santander_dev_work/SantanderDevWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
